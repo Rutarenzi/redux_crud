1aad1d1f5b2ac86b6c7a14aa410daed93cdfcd84
Rutagarama Axcel <rutagaramaaxcel@gmail.com>
2023-04-17 21:40:05 +0200

    get user from endpoint

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import store from "../src/redux/store"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { fetchUsers } from './redux/usersSlice';
 
+store.dispatch(fetchUsers());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
   <Provider store={store}>
     <App />
